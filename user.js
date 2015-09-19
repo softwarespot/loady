0ff19aa51e0eb5413de063187bdde52759c42026
softwarespot <softwarespotapps@gmail.com>
2015-09-19 13:39:44 +0300

    Loaded modules updated to ES2015

--- user.js
+++ user.js
@@ -1,11 +1,11 @@
 // IIFE - Immediately Invoked Function Expression (no global variables are leaked, apart from user of course)
-(function (global) {
+((global) => {
     function init() {
         global.alert('Init: The following output was loaded directly from user.js, but you will notice there is no script tag for loading this JavaScript file in the HTML source.');
     }
 
     // Public API
-    var _publicAPI = {
+    let _publicAPI = {
         init: init
     };
 
